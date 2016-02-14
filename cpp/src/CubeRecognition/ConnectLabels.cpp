9f5466cb42abc8b79cb06c9a9d360e220f50a06d
Rasmus Göransson <rasmusgo@volumental.com>
2016-02-14 21:27:41 +0100

    Add scaffolding for ConnectLabels

---
+++ cpp/src/CubeRecognition/ConnectLabels.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+
+#include <opencv2/highgui.hpp>
+#include <opencv2/opencv.hpp>
+
+#include "ConnectLabels.hpp"
+
+/**
+ * Connect labels by associating neighbors in a connected components search.
+ */
+void connectLabels(const std::vector<Label>& labels)
+{
+
+}
---
+++ cpp/src/CubeRecognition/ConnectLabels.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <opencv2/core/mat.hpp>
+
+#include "Label.hpp"
+
+void connectLabels(const std::vector<Label>& labels);
