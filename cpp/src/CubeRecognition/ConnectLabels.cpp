d148e3db1896507d598bf3fc5628ec5073eb9826
Rasmus Göransson <rasmusgo@volumental.com>
2016-02-14 23:36:11 +0100

    nit

--- cpp/src/CubeRecognition/ConnectLabels.cpp
+++ cpp/src/CubeRecognition/ConnectLabels.cpp
@@ -39,9 +39,9 @@ void connectLabels(const std::vector<Label>& labels)
         for (const auto& label : group)
         {
             min_x = std::min(min_x, label.native_rect.x);
-            max_x = std::max(max_x, label.native_rect.br().x);
+            max_x = std::max(max_x, label.native_rect.x + label.native_rect.width);
             min_y = std::min(min_y, label.native_rect.y);
-            max_y = std::max(max_y, label.native_rect.br().y);
+            max_y = std::max(max_y, label.native_rect.y + label.native_rect.height);
         }
         cv::Mat3b canvas(cv::Size(max_x - min_x, max_y - min_y), cv::Vec3b(0,0,0));
         for (const auto& label : group)
@@ -62,8 +62,7 @@ void connectLabels(const std::vector<Label>& labels)
             std::stringstream text;
             text << id_x << "," << id_y;
             cv::Point text_position(label.native.x - min_x, label.native.y - min_y);
-            int baseline;
-            cv::Size text_size = cv::getTextSize(text.str(), cv::FONT_HERSHEY_PLAIN, 1.0, 1, &baseline);
+            cv::Size text_size = cv::getTextSize(text.str(), cv::FONT_HERSHEY_PLAIN, 1.0, 1, NULL);
             text_position.x -= text_size.width / 2;
             text_position.y += text_size.height / 2;
             cv::putText(canvas, text.str(), text_position, cv::FONT_HERSHEY_PLAIN, 1.0, colors[i]);
