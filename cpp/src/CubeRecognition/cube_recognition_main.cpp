449ca7144c93b6c82cc85b5c18958d4d57e47c33
Rasmus Göransson <rasmus@volumental.com>
2017-07-16 12:28:25 +0200

    Print position of cubes in printMostLikelyCubes

--- cpp/src/CubeRecognition/cube_recognition_main.cpp
+++ cpp/src/CubeRecognition/cube_recognition_main.cpp
@@ -268,7 +268,9 @@ void printMostLikelyCubes(const std::vector<ProbabalisticCube>& cube_hypotheses)
         const ProbabalisticCube& cube = cube_hypotheses[i];
         const std::string permutation = cube.cube_permutation.to_String();
         const double likelihood_percent = exp(cube.log_likelihood) * 100.0;
-        printf("%d: %s %3.5f%%\n", i, permutation.c_str(), likelihood_percent);
+        printf("%d: %s (%4.1f, %4.1f, %4.1f) %3.5f%%\n", i, permutation.c_str(),
+            cube.pose_estimate[0], cube.pose_estimate[1], cube.pose_estimate[2],
+            likelihood_percent);
     }
 }
 
