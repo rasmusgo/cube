3c30806d6f0d293f1b37e4cb9960a488c7313b42
Rasmus Göransson <rasmus@volumental.com>
2018-05-10 20:56:21 +0200

    OpticalFlow: Use 8 dimesional feature vector per pixel
    color, derivatives and structure tensor

--- cpp/src/CubeRecognition/OpticalFlow.cpp
+++ cpp/src/CubeRecognition/OpticalFlow.cpp
@@ -11,17 +11,68 @@ const size_t NUM_LEVELS = 5;
 const size_t MAX_LEVEL = NUM_LEVELS - 1;
 const float INVERSE_SOBEL_SCALE_FACTOR = 1.0f / 8.0f;
 
+const size_t NUM_FEATURE_DIMENSIONS = 8;
+using FeatureVec = cv::Vec<float, NUM_FEATURE_DIMENSIONS>;
+using FeatureMat = cv::Mat_<FeatureVec>;
+const float DERIVATIVES_WEIGHT = 10.0f;
+
 } // namespace
 
-std::array<cv::Mat3b, NUM_LEVELS> createPyramid(const cv::Mat3b& image)
+std::array<FeatureMat, NUM_LEVELS> createPyramid(const cv::Mat3b& image)
 {
-    std::array<cv::Mat3b, NUM_LEVELS> pyramid;
-    pyramid[0] = image;
+    std::array<FeatureMat, NUM_LEVELS> pyramid;
+
+    // The first three channels are the color image.
+    const cv::Mat3f image_float = cv::Mat3f(image) * (1.0f / 255.0f);
+
+    // Then the derivatives of intensity.
+    cv::Mat1f image_gray;
+    cv::cvtColor(image_float, image_gray, CV_BGR2GRAY);
+    cv::Mat1f dx;
+    cv::Mat1f dy;
+    cv::Sobel(image_gray, dx, CV_32F, 1, 0, 3, INVERSE_SOBEL_SCALE_FACTOR * DERIVATIVES_WEIGHT);
+    cv::Sobel(image_gray, dy, CV_32F, 0, 1, 3, INVERSE_SOBEL_SCALE_FACTOR * DERIVATIVES_WEIGHT);
+
+    // Then the structure tensor.
+    const cv::Size kernel_size(3,3);
+    cv::Mat1f sxx = dx.mul(dx);
+    cv::Mat1f sxy = dx.mul(dy);
+    cv::Mat1f syy = dy.mul(dy);
+    cv::blur(sxx, sxx, kernel_size);
+    cv::blur(sxy, sxy, kernel_size);
+    cv::blur(syy, syy, kernel_size);
+
+    // Merge the channels to form the image of feature vectors.
+    const std::vector<cv::Mat> image_array = {
+        image_float,
+        dx, dy,
+        sxx, sxy, syy,
+    };
+    std::vector<int> from_to;
+    for (int i = 0; i < NUM_FEATURE_DIMENSIONS; ++i)
+    {
+        from_to.push_back(i);
+        from_to.push_back(i);
+    }
+    pyramid[0] = FeatureMat(image.size());
+    cv::mixChannels(image_array, pyramid[0], from_to.data(), NUM_FEATURE_DIMENSIONS);
+
+    // Create a pyramid of feature images.
     for (size_t i = 1; i < pyramid.size(); ++i)
     {
         cv::pyrDown(pyramid[i - 1], pyramid[i]);
     }
     return pyramid;
+}
+
+cv::Mat3f mat3fFromFeatureVec(const FeatureMat& image)
+{
+    std::vector<cv::Mat> channels;
+    cv::split(image, channels);
+    channels.resize(3);
+    cv::Mat3f out;
+    cv::merge(channels, out);
+    return out;
 }
 
 cv::Mat2f createAbsoluteMapFromRelative(const cv::Mat2f& offset)
@@ -38,44 +89,44 @@ cv::Mat2f createAbsoluteMapFromRelative(const cv::Mat2f& offset)
     return mapping;
 }
 
-void innerLoop(const cv::Mat3b& a, const cv::Mat3b& b, cv::Mat2f& flow, int i)
+void innerLoop(const FeatureMat& a, const FeatureMat& b, cv::Mat2f& flow, int i)
 {
     // Compute lookup tables
     const cv::Mat2f map_a_from_mid = createAbsoluteMapFromRelative(-flow);
     const cv::Mat2f map_b_from_mid = createAbsoluteMapFromRelative(flow);
 
     // Compute gradients for a
-    cv::Mat3f a_dx;
-    cv::Mat3f a_dy;
+    FeatureMat a_dx;
+    FeatureMat a_dy;
     cv::Sobel(a, a_dx, CV_32F, 1, 0, 3, INVERSE_SOBEL_SCALE_FACTOR);
     cv::Sobel(a, a_dy, CV_32F, 0, 1, 3, INVERSE_SOBEL_SCALE_FACTOR);
 
     // Map image and gradients from image a into intermediate image
-    cv::Mat3f dx_from_a;
+    FeatureMat dx_from_a;
     cv::remap(a_dx, dx_from_a, map_a_from_mid, cv::noArray(), cv::INTER_LINEAR);
-    cv::Mat3f dy_from_a;
+    FeatureMat dy_from_a;
     cv::remap(a_dy, dy_from_a, map_a_from_mid, cv::noArray(), cv::INTER_LINEAR);
-    cv::Mat3b image_from_a;
+    FeatureMat image_from_a;
     cv::remap(a, image_from_a, map_a_from_mid, cv::noArray(), cv::INTER_LINEAR);
 
     // Compute gradients for b
-    cv::Mat3f b_dx;
-    cv::Mat3f b_dy;
+    FeatureMat b_dx;
+    FeatureMat b_dy;
     cv::Sobel(b, b_dx, CV_32F, 1, 0, 3, INVERSE_SOBEL_SCALE_FACTOR);
     cv::Sobel(b, b_dy, CV_32F, 0, 1, 3, INVERSE_SOBEL_SCALE_FACTOR);
 
     // Map image and gradients from image b into intermediate image
-    cv::Mat3f dx_from_b;
+    FeatureMat dx_from_b;
     cv::remap(b_dx, dx_from_b, map_b_from_mid, cv::noArray(), cv::INTER_LINEAR);
-    cv::Mat3f dy_from_b;
+    FeatureMat dy_from_b;
     cv::remap(b_dy, dy_from_b, map_b_from_mid, cv::noArray(), cv::INTER_LINEAR);
-    cv::Mat3b image_from_b;
+    FeatureMat image_from_b;
     cv::remap(b, image_from_b, map_b_from_mid, cv::noArray(), cv::INTER_LINEAR);
 
     // Estimate flow
-    const cv::Mat3f delta = cv::Mat3f(image_from_b) - cv::Mat3f(image_from_a);
-    const cv::Mat3f delta_dx = cv::Mat3f(dx_from_b) + cv::Mat3f(dx_from_a);
-    const cv::Mat3f delta_dy = cv::Mat3f(dy_from_b) + cv::Mat3f(dy_from_a);
+    const FeatureMat delta = FeatureMat(image_from_b) - FeatureMat(image_from_a);
+    const FeatureMat delta_dx = FeatureMat(dx_from_b) + FeatureMat(dx_from_a);
+    const FeatureMat delta_dy = FeatureMat(dy_from_b) + FeatureMat(dy_from_a);
 
     for (int row = 0; row < delta.rows; ++row)
     {
@@ -85,10 +136,12 @@ void innerLoop(const cv::Mat3b& a, const cv::Mat3b& b, cv::Mat2f& flow, int i)
             const auto& dx = delta_dx(row, col);
             const auto& dy = delta_dy(row, col);
 
-            const cv::Matx32f J(
-                dx[0], dy[0],
-                dx[1], dy[1],
-                dx[2], dy[2]);
+            cv::Matx<float, NUM_FEATURE_DIMENSIONS, 2> J;
+            for (int i = 0; i < NUM_FEATURE_DIMENSIONS; ++i)
+            {
+                J(i, 0) = dx[i];
+                J(i, 1) = dy[i];
+            }
             /*
             h_2_1
             d_3_1
@@ -101,7 +154,7 @@ void innerLoop(const cv::Mat3b& a, const cv::Mat3b& b, cv::Mat2f& flow, int i)
             (Jt_2_3 * J_3_2).inv() * -Jt_2_3 * d_3_1 = h_2_1
             - JtJ_2_2.inv() * Jt_2_3 * d_3_1 = h_2_1
             */
-            const float reg = 10.0f;
+            const float reg = 0.01f;
             const cv::Matx22f regularization(
                 reg, 0.0f,
                 0.0f, reg);
@@ -113,26 +166,28 @@ void innerLoop(const cv::Mat3b& a, const cv::Mat3b& b, cv::Mat2f& flow, int i)
     {
         char label[] = "intermediate[0]";
         label[14] = '0' + i;
-        const cv::Mat3b intermediate = (cv::Mat3f(image_from_b) + cv::Mat3f(image_from_a)) * 0.5f;
+        const cv::Mat3f intermediate = (
+            mat3fFromFeatureVec(image_from_b) +
+            mat3fFromFeatureVec(image_from_a)) * 0.5f;
         cv::imshow(label, intermediate);
     }
 
     {
         char label[] = "delta[0]";
         label[6] = '0' + i;
-        cv::imshow(label, 0.5 + delta * 0.01);
+        cv::imshow(label, 0.5f + mat3fFromFeatureVec(delta) * 0.5f);
     }
 
     {
         char label[] = "delta_dx[0]";
         label[9] = '0' + i;
-        cv::imshow(label, 0.5 + delta_dx * 0.01);
+        cv::imshow(label, 0.5f + mat3fFromFeatureVec(delta_dx) * 0.2f);
     }
 
     {
         char label[] = "delta_dy[0]";
         label[9] = '0' + i;
-        cv::imshow(label, 0.5 + delta_dy * 0.01);
+        cv::imshow(label, 0.5f + mat3fFromFeatureVec(delta_dy) * 0.2f);
     }
 
     {
