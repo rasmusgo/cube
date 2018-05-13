41b6f8e247c8308ab8a1ec436917c0fb4ebcced4
Rasmus Göransson <rasmus@volumental.com>
2018-05-13 21:38:45 +0200

    OpticalFlow: Single difference of Gaussians + packPyramids

--- cpp/src/CubeRecognition/OpticalFlow.cpp
+++ cpp/src/CubeRecognition/OpticalFlow.cpp
@@ -1,6 +1,7 @@
 #include "OpticalFlow.hpp"
 
 #include <array>
+#include <cstdlib>
 #include <vector>
 
 #include <opencv2/opencv.hpp>
@@ -9,13 +10,13 @@
 
 namespace {
 
-const size_t NUM_INNER_ITERATIONS = 2;
-const size_t MIN_LEVEL = 1;
-const size_t MAX_LEVEL = 4;
+const size_t NUM_INNER_ITERATIONS = 3;
+const size_t MIN_LEVEL = 0;
+const size_t MAX_LEVEL = 5;
 const size_t NUM_LEVELS = MAX_LEVEL + 1;
 const float INVERSE_SOBEL_SCALE_FACTOR = 1.0f / 8.0f;
 
-const size_t NUM_FEATURE_DIMENSIONS = 9;
+const size_t NUM_FEATURE_DIMENSIONS = 3;
 using FeatureVec = cv::Vec<float, NUM_FEATURE_DIMENSIONS>;
 using FeatureMat = cv::Mat_<FeatureVec>;
 
@@ -28,79 +29,93 @@ std::array<FeatureMat, NUM_LEVELS> createPyramid(const cv::Mat3b& image)
     // The first three channels are the color image.
     const cv::Mat3f image_float = cv::Mat3f(image) * (1.0f / 255.0f);
 
-    // Three levels of difference of Gaussians
-    cv::Mat3f image_g1;
-    cv::Mat3f image_g2;
-    cv::Mat3f image_g3;
-    cv::Mat3f image_g4;
-    const cv::Size kernel_size_g1(3, 3);
-    const cv::Size kernel_size_g2(13, 13);
-    const cv::Size kernel_size_g3(35, 35);
-    const cv::Size kernel_size_g4(101, 101);
-    cv::GaussianBlur(image_float, image_g1, kernel_size_g1, 0.0);
-    cv::GaussianBlur(image_float, image_g2, kernel_size_g2, 0.0);
-    cv::GaussianBlur(image_float, image_g3, kernel_size_g3, 0.0);
-    cv::GaussianBlur(image_float, image_g4, kernel_size_g4, 0.0);
-
-    // Merge the channels to form the image of feature vectors.
-    const std::vector<cv::Mat> image_array = {
-        image_g1 - image_g2,
-        image_g2 - image_g3,
-        image_g3 - image_g4,
-    };
-    std::vector<int> from_to;
-    for (int i = 0; i < NUM_FEATURE_DIMENSIONS; ++i)
-    {
-        from_to.push_back(i);
-        from_to.push_back(i);
-    }
-    pyramid[0] = FeatureMat(image.size());
-    cv::mixChannels(image_array, pyramid[0], from_to.data(), NUM_FEATURE_DIMENSIONS);
-
-    // Create a pyramid of feature images.
+    // Create a pyramid of images.
+    pyramid[0] = image_float;
     for (size_t i = 1; i < pyramid.size(); ++i)
     {
         cv::pyrDown(pyramid[i - 1], pyramid[i]);
     }
+
+    // Replace images with difference of Gaussians.
+    const cv::Size kernel_size_g1(3, 3);
+    const cv::Size kernel_size_g2(9, 9);
+    for (size_t i = 0; i < pyramid.size(); ++i)
+    {
+        cv::Mat3f image_g1;
+        cv::Mat3f image_g2;
+        cv::GaussianBlur(pyramid[i], image_g1, kernel_size_g1, 0.0);
+        cv::GaussianBlur(pyramid[i], image_g2, kernel_size_g2, 0.0);
+        pyramid[i] = image_g1 - image_g2;
+    }
+
     return pyramid;
 }
 
-cv::Mat3f part1FromFeatureVec(const FeatureMat& image)
+cv::Mat3f packPyramid(const std::array<cv::Mat3f, NUM_LEVELS>& pyramid)
 {
-    std::vector<cv::Mat> channels;
-    cv::split(image, channels);
-    channels.resize(3);
-    cv::Mat3f out;
-    cv::merge(channels, out);
-    return out;
+    const cv::Size canvas_size(
+        pyramid[0].cols + pyramid[1].cols,
+        pyramid[0].rows);
+    cv::Mat3f canvas(canvas_size, cv::Vec3f(0, 0, 0));
+
+    pyramid[0].copyTo(canvas(cv::Rect(0, 0, pyramid[0].cols, pyramid[0].rows)));
+    const int x = pyramid[0].cols;
+    int y = 0;
+    for (int i = 1; i < NUM_LEVELS; ++i)
+    {
+        const int w = pyramid[i].cols;
+        const int h = pyramid[i].rows;
+        pyramid[i].copyTo(canvas(cv::Rect(x, y, w, h)));
+        y += h;
+    }
+    return canvas;
 }
 
-cv::Mat3f part2FromFeatureVec(const FeatureMat& image)
+cv::Mat3f packPyramids(
+    const std::array<cv::Mat3f, NUM_LEVELS>& top_left,
+    const std::array<cv::Mat3f, NUM_LEVELS>& top_right,
+    const std::array<cv::Mat3f, NUM_LEVELS>& bottom_left)
 {
-    std::vector<cv::Mat> channels;
-    cv::split(image, channels);
-    channels = {
-        channels.at(3),
-        channels.at(4),
-        channels.at(5),
-    };
-    cv::Mat3f out;
-    cv::merge(channels, out);
-    return out;
-}
+    assert(!top_left[0].empty());
+    assert(!top_right[0].empty());
+    assert(!bottom_left[0].empty());
 
-cv::Mat3f part3FromFeatureVec(const FeatureMat& image)
-{
-    std::vector<cv::Mat> channels;
-    cv::split(image, channels);
-    channels = {
-        channels.at(6),
-        channels.at(7),
-        channels.at(8),
-    };
-    cv::Mat3f out;
-    cv::merge(channels, out);
-    return out;
+    for (size_t i = 0; i < NUM_LEVELS; ++i)
+    {
+        assert(top_left[i].size() == top_right[i].size());
+        assert(top_left[i].size() == bottom_left[i].size());
+    }
+
+    cv::Size max_size(0, 0);
+    {
+        int x = 0;
+        int y = 0;
+        for (size_t i = 0; i < NUM_LEVELS; ++i)
+        {
+            const int w = top_left[i].cols;
+            const int h = top_left[i].rows;
+            x += w;
+            y += h;
+            max_size.width = std::max(max_size.width, x + w);
+            max_size.height = std::max(max_size.height, y + h);
+        }
+    }
+
+    cv::Mat3f canvas(max_size, cv::Vec3f(0, 0, 0));
+
+    int x = 0;
+    int y = 0;
+    for (size_t i = 0; i < NUM_LEVELS; ++i)
+    {
+        const int w = top_left[i].cols;
+        const int h = top_left[i].rows;
+        top_left[i].copyTo(canvas(cv::Rect(x, y, w, h)));
+        top_right[i].copyTo(canvas(cv::Rect(x + w, y, w, h)));
+        bottom_left[i].copyTo(canvas(cv::Rect(x, y + h, w, h)));
+        x += w;
+        y += h;
+    }
+    return canvas;
 }
 
 cv::Mat2f createAbsoluteMapFromRelative(const cv::Mat2f& offset)
@@ -157,16 +172,17 @@ void innerLoop(const FeatureMat& a, const FeatureMat& b, cv::Mat2f& flow, int i)
     const FeatureMat delta_dy = FeatureMat(dy_from_b) + FeatureMat(dy_from_a);
 
     // Compute gradients for flow regularization
-    const float regularization_towards_zero = 0.001f;
-    const float regularization_towards_same = 0.01f;
-    const float regularization_towards_smooth = 0.1f;
+    const float regularization_towards_zero   = 0.00f;
+    const float regularization_towards_same   = 0.01f;
+    const float regularization_towards_smooth = 0.01f;
     cv::Mat2f flow_dx;
     cv::Mat2f flow_dy;
     cv::Sobel(flow, flow_dx, CV_32F, 1, 0, 3, INVERSE_SOBEL_SCALE_FACTOR);
     cv::Sobel(flow, flow_dy, CV_32F, 0, 1, 3, INVERSE_SOBEL_SCALE_FACTOR);
 
     cv::Mat2f flow_update(delta.size());
-    cv::Mat1f uncertainty(delta.size());
+    //cv::Mat1f uncertainty(delta.size());
+    #pragma omp for
     for (int row = 0; row < delta.rows; ++row)
     {
         for (int col = 0; col < delta.cols; ++col)
@@ -206,67 +222,34 @@ void innerLoop(const FeatureMat& a, const FeatureMat& b, cv::Mat2f& flow, int i)
                 + regularization_towards_smooth * flow_dy(row, col);
 
             flow_update(row, col) = JtJinv * Jtd;
-            uncertainty(row, col) = std::sqrt(JtJinv(0,0) + JtJinv(1,1));
+            //uncertainty(row, col) = std::sqrt(JtJinv(0,0) + JtJinv(1,1));
         }
     }
     flow -= flow_update;
 
-    const FeatureMat intermediate = (image_from_b + image_from_a) * 0.5f;
+    /*
     {
-        char label[] = "intermediate[0] part1";
-        label[13] = '0' + i;
-        cv::imshow(label, 0.5f + part1FromFeatureVec(intermediate) * 0.5f);
-    }
-    {
-        char label[] = "intermediate[0] part2";
-        label[13] = '0' + i;
-        cv::imshow(label, 0.5f + part2FromFeatureVec(intermediate) * 0.5f);
-    }
-    {
-        char label[] = "intermediate[0] part3";
-        label[13] = '0' + i;
-        cv::imshow(label, 0.5f + part3FromFeatureVec(intermediate) * 0.5f);
-    }
-
-    {
-        char label[] = "delta[0] part1";
+        char label[] = "delta[0]";
         label[6] = '0' + i;
-        cv::imshow(label, 0.5f + part1FromFeatureVec(delta) * 0.5f);
-    }
-
-    {
-        char label[] = "delta[0] part2";
-        label[6] = '0' + i;
-        cv::imshow(label, 0.5f + part2FromFeatureVec(delta) * 0.5f);
-    }
-
-    {
-        char label[] = "delta[0] part3";
-        label[6] = '0' + i;
-        cv::imshow(label, 0.5f + part3FromFeatureVec(delta) * 0.5f);
+        cv::imshow(label, 0.5f + delta * 0.5f);
     }
 
     {
         char label[] = "delta_dx[0]";
         label[9] = '0' + i;
-        cv::imshow(label, 0.5f + part1FromFeatureVec(delta_dx) * 0.2f);
+        cv::imshow(label, 0.5f + delta_dx * 0.2f);
     }
 
     {
         char label[] = "delta_dy[0]";
         label[9] = '0' + i;
-        cv::imshow(label, 0.5f + part1FromFeatureVec(delta_dy) * 0.2f);
+        cv::imshow(label, 0.5f + delta_dy * 0.2f);
     }
 
     {
         char label[] = "flow[0]";
         label[5] = '0' + i;
-        cv::Mat1f xy[2];
-        cv::split(flow, xy);
-        cv::Mat3f xyy;
-        cv::merge(std::vector<cv::Mat>{xy[0], xy[1], xy[1]}, xyy);
-        const float scale = 20.0f / delta.cols;
-        cv::imshow(label, 0.5 + xyy * scale);
+        cv::imshow(label, colorizeOpticalFlow(flow));
     }
 
     {
@@ -274,6 +257,7 @@ void innerLoop(const FeatureMat& a, const FeatureMat& b, cv::Mat2f& flow, int i)
         label[12] = '0' + i;
         cv::imshow(label, uncertainty * 0.1f);
     }
+    */
 }
 
 cv::Mat2f intermediateOpticalFlow(
@@ -284,6 +268,9 @@ cv::Mat2f intermediateOpticalFlow(
     assert(!b.empty());
     assert(a.cols == b.cols);
     assert(a.rows == b.rows);
+
+    cv::imshow("a", a);
+    cv::imshow("b", b);
 
     startTimer();
     const auto a_pyramid = createPyramid(a);
@@ -312,31 +299,34 @@ cv::Mat2f intermediateOpticalFlow(
     }
     stopTimer("computing flow");
 
+    startTimer();
+    std::array<cv::Mat3f, NUM_LEVELS> colorized_flow_pyramid;
+    std::array<cv::Mat3f, NUM_LEVELS> mid_pyramid;
+    for (size_t i = 0; i < NUM_LEVELS; ++i)
+    {
+        const auto& flow = flow_pyramid[i];
+        colorized_flow_pyramid[i] = colorizeOpticalFlow(flow);
+        const cv::Mat2f map_a_from_mid = createAbsoluteMapFromRelative(-flow);
+        const cv::Mat2f map_b_from_mid = createAbsoluteMapFromRelative(flow);
+        cv::Mat3f image_from_a;
+        cv::Mat3f image_from_b;
+        cv::remap(a_pyramid[i], image_from_a, map_a_from_mid, cv::noArray(), cv::INTER_LINEAR);
+        cv::remap(b_pyramid[i], image_from_b, map_b_from_mid, cv::noArray(), cv::INTER_LINEAR);
+        mid_pyramid[i] = (image_from_a + image_from_b) * 0.5f;
+    }
+    cv::imshow("feature pyramids", 0.5f + packPyramids(a_pyramid, b_pyramid, mid_pyramid) * 0.5f);
+    cv::imshow("flow pyramid", packPyramid(colorized_flow_pyramid));
+    stopTimer("visualizing flow");
+
     return flow_pyramid[MIN_LEVEL];
 }
 
 cv::Mat3f colorizeOpticalFlow(const cv::Mat2f& flow)
 {
-    cv::Mat1f xy[2]; // X,Y
+    cv::Mat1f xy[2];
     cv::split(flow, xy);
-
-    // Calculate angle and magnitude
-    cv::Mat1f magnitude;
-    cv::Mat1f angle;
-    cv::cartToPolar(xy[0], xy[1], magnitude, angle, true);
-
-    magnitude *= 50.0 / flow.cols;
-
-    // Build hsv image
-    cv::Mat1f _hsv[3];
-    cv::Mat3f hsv;
-    _hsv[0] = angle;
-    _hsv[1] = cv::Mat::ones(angle.size(), CV_32F);
-    _hsv[2] = magnitude;
-    cv::merge(_hsv, 3, hsv);
-
-    // Convert to BGR and show
-    cv::Mat3f bgr;
-    cv::cvtColor(hsv, bgr, cv::COLOR_HSV2BGR);
-    return bgr;
+    cv::Mat3f xyy;
+    cv::merge(std::vector<cv::Mat>{xy[0], xy[1] * 0, xy[1]}, xyy);
+    const float scale = 20.0f / std::max(flow.cols, flow.rows);
+    return 0.5 + xyy * scale;
 }
